f8cb03bf880d69e9304e9b301190452cd8f6c4e4
Rockingcool <aadhavan@twomorecents.org>
2024-08-15 12:28:12 -0500

    Wrote script to create release builds

---
+++ create_release_builds.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+POSSIBLE_GOOS=( "windows" "linux" "darwin" )
+POSSIBLE_GOARCH=( "amd64" "arm64" )
+
+for OS in "${POSSIBLE_GOOS[@]}"; do
+	for ARCH in "${POSSIBLE_GOARCH[@]}"; do
+		FOLDER_NAME="ccat-$OS-$ARCH"
+		mkdir "${FOLDER_NAME}"
+		GOOS=$OS GOARCH=$ARCH go build -o "${FOLDER_NAME}/"
+		zip -r "${FOLDER_NAME}" "${FOLDER_NAME}"
+		rm -r "${FOLDER_NAME}"
+	done
+done
+
